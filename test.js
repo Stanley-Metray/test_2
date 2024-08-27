19bf28212962e6c4f44aa69ad85ba2c1a87adc59
Stanley Metray <stanleymetray@gmail.com>
2024-08-27 21:49:22 +0530

    Merge sorted array, practice

--- test.js
+++ test.js
@@ -1,39 +1,54 @@
 console.clear();
 
-function removeElement(nums, val) {
-    let k = 0;
-    
-    for(let i=0; i<nums.length; i++)
+function merge(nums1, m, nums2, n) {
+    let k = m+n-1;
+
+    let i=m-1; j=n-1;
+
+    while(i>=0 && j>=0)
     {
-        if(nums[i]!==val)
-            nums[k++] = nums[i];
+        if(nums2[j]>=nums1[i])
+        {
+            nums1[k] = nums2[j];
+            k--; j--;
+        }
+            nums1[k] = nums1[i];
+            k--; i--;
     }
-    
-    return k;
+
+    if(j>=0)
+        while(j>=0)
+            nums1[k--] = nums2[j--];
+
 }
 
 
 // Test Case 1
-let nums = [3, 2, 2, 3];
-let val = 3;
-let k = removeElement(nums, val);
-console.log(nums.slice(0, k));  // Expected Output: [2, 2]
+let nums1 = [1, 2, 3, 0, 0, 0];
+let nums2 = [2, 5, 6];
+merge(nums1, 3, nums2, 3);
+console.log(nums1); // Expected output: [1, 2, 2, 3, 5, 6]
 
 // Test Case 2
-nums = [0, 1, 2, 2, 3, 0, 4, 2];
-val = 2;
-k = removeElement(nums, val);
-console.log(nums.slice(0, k));  // Expected Output: [0, 1, 3, 0, 4]
+nums1 = [1];
+nums2 = [];
+merge(nums1, 1, nums2, 0);
+console.log(nums1); // Expected output: [1]
 
 // Test Case 3
-nums = [1, 1, 1, 1];
-val = 1;
-k = removeElement(nums, val);
-console.log(nums.slice(0, k));  // Expected Output: []
+nums1 = [0];
+nums2 = [1];
+merge(nums1, 0, nums2, 1);
+console.log(nums1); // Expected output: [1]
 
 // Test Case 4
-nums = [4, 5, 6];
-val = 7;
-k = removeElement(nums, val);
-console.log(nums.slice(0, k));  // Expected Output: [4, 5, 6]
+nums1 = [2, 0];
+nums2 = [1];
+merge(nums1, 1, nums2, 1);
+console.log(nums1); // Expected output: [1, 2]
 
+// Test Case 5
+nums1 = [4, 5, 6, 0, 0, 0];
+nums2 = [1, 2, 3];
+merge(nums1, 3, nums2, 3);
+console.log(nums1); // Expected output: [1, 2, 3, 4, 5, 6]
