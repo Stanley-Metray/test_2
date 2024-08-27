e193ba7271448ab8a686b4e4a6f38d6a065d8fda
Stanley Metray <stanleymetray@gmail.com>
2024-08-27 21:55:02 +0530

    Remove element from array, IN PLACE

--- test.js
+++ test.js
@@ -1,54 +1,45 @@
 console.clear();
 
-function merge(nums1, m, nums2, n) {
-    let k = m+n-1;
+function removeElement(nums, val) {
+   let k = 0;
 
-    let i=m-1; j=n-1;
-
-    while(i>=0 && j>=0)
-    {
-        if(nums2[j]>=nums1[i])
+   for(let i=0; i<nums.length; i++)
+   {
+        if(nums[i]!==val)
         {
-            nums1[k] = nums2[j];
-            k--; j--;
+            nums[k++] = nums[i];
         }
-            nums1[k] = nums1[i];
-            k--; i--;
-    }
+   }
 
-    if(j>=0)
-        while(j>=0)
-            nums1[k--] = nums2[j--];
-
+   return k;
 }
 
 
-// Test Case 1
-let nums1 = [1, 2, 3, 0, 0, 0];
-let nums2 = [2, 5, 6];
-merge(nums1, 3, nums2, 3);
-console.log(nums1); // Expected output: [1, 2, 2, 3, 5, 6]
+let nums1 = [3, 2, 2, 3];
+let val1 = 3;
+let k1 = removeElement(nums1, val1);
+console.log(nums1.slice(0, k1)); // Expected output: 2, [2, 2]
 
 // Test Case 2
-nums1 = [1];
-nums2 = [];
-merge(nums1, 1, nums2, 0);
-console.log(nums1); // Expected output: [1]
+let nums2 = [0, 1, 2, 2, 3, 0, 4, 2];
+let val2 = 2;
+let k2 = removeElement(nums2, val2);
+console.log(nums2.slice(0, k2)); // Expected output: 5, [0, 1, 3, 0, 4]
 
 // Test Case 3
-nums1 = [0];
-nums2 = [1];
-merge(nums1, 0, nums2, 1);
-console.log(nums1); // Expected output: [1]
+let nums3 = [2, 2, 2, 2];
+let val3 = 2;
+let k3 = removeElement(nums3, val3);
+console.log(nums3.slice(0, k3)); // Expected output: 0, []
 
 // Test Case 4
-nums1 = [2, 0];
-nums2 = [1];
-merge(nums1, 1, nums2, 1);
-console.log(nums1); // Expected output: [1, 2]
+let nums4 = [1, 2, 3, 4, 5];
+let val4 = 6;
+let k4 = removeElement(nums4, val4);
+console.log(nums4.slice(0, k4)); // Expected output: 5, [1, 2, 3, 4, 5]
 
 // Test Case 5
-nums1 = [4, 5, 6, 0, 0, 0];
-nums2 = [1, 2, 3];
-merge(nums1, 3, nums2, 3);
-console.log(nums1); // Expected output: [1, 2, 3, 4, 5, 6]
+let nums5 = [4, 5, 6, 6, 7, 8];
+let val5 = 6;
+let k5 = removeElement(nums5, val5);
+console.log(nums5.slice(0, k5)); // Expected output: 4, [4, 5, 7, 8]
